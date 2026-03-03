0e9ad9508f3f10b33dd2c3c4acbf714691f1329f
Yelisieiev Yevhenii <147833843+MPMotorobot@users.noreply.github.com>
2026-03-03 18:16:09 +0100

    Add files via upload

---
+++ js/translation.js
@@ -0,0 +1,149 @@
+const translations = {
+    en: {
+        title: "Profile frontend developer",
+
+        myName: "Yelisieiev Yevhenii",
+        description: "Beginner Frontend Developer",
+
+        nav_about: "About me",
+        nav_skills: "Skills",
+        nav_projects: "Projects",
+        nav_contacts: "Contacts",
+
+        about_title: "About me",
+        about_text: "I'm a beginner front-end developer. I study and live in Poland. I'm studying computer science and programming at a technical school. ",
+        
+        skills: "Skills",
+        languages: "Languages",
+
+        lang_en: "English",
+        lang_ua: "Ukrainian",
+        lang_pl: "Polish",
+        lang_ru: "Russian",
+        speaking: "Speaking",
+
+        project_title: "My Projects",
+        minecraft: "Minecraft Server Website",
+        game: "Site with top's games",
+        calculator: "Calculator",
+
+        aside: "I live in Częstochowa, Poland. I'm a boxer and go to the gym. I'm 18 years old.",
+        resume: "My resume"
+    },
+    ru: {
+        title: "Профиль фронтенд-разработчика",
+
+        myName: "Елисиеев Евгений",
+        description: "Начинающий фронтенд-разработчик",
+
+        nav_about: "Обо мне",
+        nav_skills: "Навыки",
+        nav_projects: "Проекты",
+        nav_contacts: "Контакты",
+
+        about_title: "Обо мне",
+        about_text: "Я начинающий фронтенд-разработчик. Я учусь и живу в Польше. Я изучаю информатику и программирование в техническом училище.",
+        
+        skills: "Навыки",
+        languages: "Владение языками",
+
+        lang_en: "Английский",
+        lang_ua: "Украинский",
+        lang_pl: "Польский",
+        lang_ru: "Русский",
+        speaking: "Говорящий",
+
+        project_title: "Мои проекты",
+        minecraft: "Сайт сервера Minecraft",
+        game: "Сайт с лучшими играми",
+        calculator: "Калькулятор",
+        
+        aside: "Я живу в Ченстохове, Польша. Я боксер и хожу в спортзал. Мне 18 лет.",
+        resume: "Моё резюме"
+    },
+    ua: {
+        title: "Профіль фронтенд-розробника",
+
+        myName: "Єлісєєв Євгеній",
+        description: "Фронтенд-розробник-початківець",
+
+        nav_about: "Про мене",
+        nav_skills: "Навички",
+        nav_projects: "Проєкти",
+        nav_contacts: "Контакти",
+
+        about_title: "Про мене",
+        about_text: "Я фронтенд-розробник-початківець. Я навчаюся та живу в Польщі. Вивчаю інформатику та програмування в технікумі. ",
+
+        skills: "Навички",
+        languages: "Мови",
+
+        lang_en: "Англійська",
+        lang_ua: "Українська",
+        lang_pl: "Польська",
+        lang_ru: "Російська",
+        speaking: "Розмовна мова",
+
+        project_title: "Мої проєкти",
+        minecraft: "Веб-сайт сервера Minecraft",
+        game: "Сайт з топовими іграми",
+        calculator: "Калькулятор",
+
+        aside: "Я живу в Ченстохова, Польща. Я боксер та ходжу в спортзал. Мені 18 років.",
+        resume: "Моє резюме"
+    },
+    pl: {
+        title: "Profil frontend developera",
+
+        myName: "Yelisieiev Yevhenii",
+        description: "Początkujący frontend developer",
+
+        nav_about: "O mnie",
+        nav_skills: "Umiejętności",
+        nav_projects: "Projekty",
+        nav_contacts: "Kontakty",
+
+        about_title: "O mnie",
+        about_text: "Jestem początkującym frontend developerem. Studiuję i mieszkam w Polsce. Studiuję informatykę i programowanie w technikum. ",
+        
+        skills: "Umiejętności",
+        languages: "Jezyki",
+
+        lang_en: "Angielski",
+        lang_ua: "Ukraiński",
+        lang_pl: "Polski",
+        lang_ru: "Rosyjski",
+        speaking: "Mówiący",
+
+        project_title: "Moje projekty",
+        minecraft: "Strona serwera Minecraft",
+        game: "Strona z Gry topowe",
+        calculator: "Kalkulator",
+
+        aside: "Mieszkam w Częstochowie. Jestem bokserem i chodzę na siłownię. Mam 18 lat.",
+        resume: "Moje CV"
+    }
+};
+
+const langSelect = document.getElementById("language-select");
+
+const savedLang = localStorage.getItem("language") || "en";
+langSelect.value = savedLang;
+setLanguage(savedLang);
+
+langSelect.addEventListener("change", (e) => {
+    const lang = e.target.value;
+    localStorage.setItem("language", lang);
+    setLanguage(lang);
+});
+
+function setLanguage(lang) {
+  document.querySelectorAll("[data-i18n]").forEach(element => {
+    const key = element.getAttribute("data-i18n");
+    if (key in translations[lang]) {
+        element.textContent = translations[lang][key];
+    }
+  });
+
+  document.documentElement.lang = lang;
+}
